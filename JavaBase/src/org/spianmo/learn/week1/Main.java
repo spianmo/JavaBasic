5fac17c4c0366933ae8940bdc7283bb7b4687857
Finger <Finger@spianmo.com>
2020-10-05 16:15:30 +0800

    初始化项目结构

---
+++ JavaBase/src/org/spianmo/learn/week1/Main.java
@@ -0,0 +1,10 @@
+package org.spianmo.learn.week1;
+
+/**
+ * @ClassName Main
+ * @Description TODO
+ * @Author Finger
+ * @Date 10/5/2020
+ **/
+public class Main {
+}
---
+++ JavaBase/src/org/spianmo/learn/week2/Main.java
@@ -0,0 +1,8 @@
+package org.spianmo.learn.week2;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
